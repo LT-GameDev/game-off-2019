1efdc3eba33d7637cdb8d409aa1ddf3074169006
Cem Kucukhasanoglu <kucukhasanoglu.cem@gmail.com>
2019-11-03 17:01:16 +0300

    cinemachine and player camera

---
+++ Assets/Game/Scripts/Components/Cameras/DefaultPlayerCameraTarget.cs
@@ -0,0 +1,30 @@
+﻿#pragma warning disable 649
+
+using UnityEngine;
+
+namespace Game.Cameras
+{
+    public class DefaultPlayerCameraTarget : MonoBehaviour
+    {
+        [SerializeField] private Transform follow;
+        [SerializeField] private Transform look;
+
+        private PlayerCamera playerCamera;
+
+        private void OnEnable()
+        {
+            PlayerCamera.SetTarget(follow, look);
+        }
+
+        private PlayerCamera PlayerCamera
+        {
+            get
+            {
+                if (!playerCamera)
+                    playerCamera = FindObjectOfType<PlayerCamera>();
+
+                return playerCamera;
+            }
+        }
+    }
+}
