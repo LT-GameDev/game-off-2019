52cb4b1ee9eb1153c47b77e22f5ac2d8d6f0431e
Cem Kucukhasanoglu <kucukhasanoglu.cem@gmail.com>
2019-11-03 20:17:12 +0300

    add canInteract and cannotInteract events

--- Assets/Game/Scripts/Components/Interactor.cs
+++ Assets/Game/Scripts/Components/Interactor.cs
@@ -2,11 +2,15 @@
 
 using Game.Interactions;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Game.Components
 {
     public class Interactor : MonoBehaviour
     {
+        [SerializeField] private UnityEvent canInteract;
+        [SerializeField] private UnityEvent cannotInteract;
+        
         private IInteractable interactable;
 
         public void Interact()
@@ -17,14 +21,21 @@ namespace Game.Components
         private void OnTriggerEnter(Collider collider)
         {
             interactable = collider.GetComponent<IInteractable>();
+            
+            if (interactable != null)
+                canInteract.Invoke();
         }
 
         private void OnTriggerExit(Collider collider)
         {
             var interactableInstance = collider.GetComponent<IInteractable>();
-            
+
             if (interactableInstance == interactable)
+            {
                 interactable = null;
+                
+                cannotInteract.Invoke();
+            }
         }
     }
 }
