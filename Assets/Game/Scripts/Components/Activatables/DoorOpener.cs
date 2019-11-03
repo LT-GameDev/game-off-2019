b1051200fe4e7fc16eab21e15db70c036708c1e1
Cem Kucukhasanoglu <kucukhasanoglu.cem@gmail.com>
2019-11-03 21:41:35 +0300

    move door opener to components/activatables

---
+++ Assets/Game/Scripts/Components/Activatables/DoorOpener.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.Interactions;
+using UnityEngine;
+
+namespace Game.Components.Activatables
+{
+    public class DoorOpener : Activatable, IInteractable
+    {
+        public float smooth = 2f;
+        public float doorOpenAngle = 90f;
+        public float doorCloseAngle = 0f;
+        public bool isOpen = false;
+
+        public void Notify(bool notifiedState)
+        {
+            if (notifiedState)
+            {
+                Debug.Log($"Press 'F' to open {gameObject.name}");
+            }
+        }
+    
+        public void Interact()
+        {
+            Activate(isOpen = !isOpen);
+        }
+
+        public override void Activate(bool state)
+        {
+            isOpen = state;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isOpen)
+            {
+                Quaternion targetRotation = Quaternion.Euler(0, 0, doorOpenAngle);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+            }
+            else
+            {
+                Quaternion targetRotation2 = Quaternion.Euler(0, 0, doorCloseAngle);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, smooth * Time.deltaTime);
+            }
+        }
+    }
+}
