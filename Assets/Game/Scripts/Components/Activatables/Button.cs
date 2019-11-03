904203ec9776acf5fe70f0af1f6840d33a662eb0
Romans <50585282+rkononovs@users.noreply.github.com>
2019-11-03 21:51:03 +0000

    Added makeshift button script as well as added dummy button on scene

---
+++ Assets/Game/Scripts/Components/Activatables/Button.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.Interactions;
+using Game.Components.Activatables;
+using UnityEngine;
+
+public class Button : MonoBehaviour, IInteractable
+{
+    public bool isPressed = false;
+    public DoorOpener isOpened;
+
+    public void Notify(bool notifiedState)
+    {
+        if (notifiedState)
+        {
+            Debug.Log($"Press 'F' to press {gameObject.name}");
+        }
+    }
+
+    public void Interact()
+    {
+        Activate(isPressed = !isPressed);
+    }
+
+    public void Activate(bool state)
+    {
+        isPressed = state;
+        if (isPressed && !isOpened.isOpen) // if button is pressed and door is not closed
+        {
+            isOpened.isOpen = isPressed;
+            isPressed = false;
+            Debug.Log($"{gameObject.name} is opened");
+        }
+        else
+        {
+            isOpened.isOpen = !isPressed;
+            isPressed = false;
+            Debug.Log($"{gameObject.name} is closed");
+        }
+    }
+}
