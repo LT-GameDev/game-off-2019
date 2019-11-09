9677926f3d0bfeeea21c0f34e8e289a7549c9c94
Cem Kucukhasanoglu <kucukhasanoglu.cem@gmail.com>
2019-11-09 18:00:14 +0300

    compile fix

--- Assets/Game/Scripts/Components/Movement/PlayerMovementController.cs
+++ Assets/Game/Scripts/Components/Movement/PlayerMovementController.cs
@@ -4,9 +4,6 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 using Game.Components.Movement.DefaultMovement;
-using Game.Components.Movement.MovementControl;
-using Game.Components.Movement.Wallrun;
-using Game.Models.Movement.Contexts;
 using Game.Utility;
 
 namespace Game.Components.Movement
@@ -26,7 +23,6 @@ namespace Game.Components.Movement
         
         [Header("Movement Modes")]
         [SerializeField] private DefaultMovementLogic defaultMovement;
-        [SerializeField] private WallMovementLogic wallMovement;
 
         private Transform myTransform;
         private Action currentMovementUpdate;
@@ -39,8 +35,7 @@ namespace Game.Components.Movement
             myTransform = transform;
             
             movementModes = new Dictionary<MovementMode, Action> {
-                { MovementMode.Humanoid, DefaultMovementUpdate },
-                { MovementMode.WallMovement, WallMovementUpdate }
+                { MovementMode.Humanoid, DefaultMovementUpdate }
             };
 
             SwitchMode(default);
@@ -51,32 +46,6 @@ namespace Game.Components.Movement
             currentMovementUpdate?.Invoke();
         }
 
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.relativeVelocity.magnitude < wallRunBeginSpeed)
-                return;
-            
-            var groundAxis  = new Vector3(1, 0, 1);
-
-            var normal  = collision.contacts[0].normal;
-            var tangent = Vector3.Cross(normal, Vector3.forward);
-            var up      = myTransform.up;
-            
-            // Get angle between surface tangent and x axis on y axis
-            var tangentToForwrdAngle    = Vector3.SignedAngle(Vector3.right, normal, Vector3.up);
-            var canSwitchToWallMovement = Mathf.Abs(tangentToForwrdAngle) > 180 - wallRunBeginAngle && wallMovement.Context.Sprinting && movementInput.magnitude > 0;
-            
-            this.Log().Debug(
-                $"angle: {tangentToForwrdAngle}. can switch to wall movement: {canSwitchToWallMovement} (abs(tangentToForwardAngle) < wallRunBeginAngle)", 
-                nameof(OnCollisionEnter));
-
-            // If this angle is higher than the wall run begin angle, we cannot wall run!
-            if (!canSwitchToWallMovement)
-                return;
-            
-            SwitchMode(MovementMode.WallMovement);
-        }
-
         public void Move(Vector2 movementInput)
         {
             this.movementInput = movementInput;
@@ -85,13 +54,11 @@ namespace Game.Components.Movement
         public void Jump()
         {
             defaultMovement.Context.Jumping = true;
-            wallMovement.Context.Jump = true;
         }
 
         public void SetSprinting(bool sprinting)
         {
             defaultMovement.Context.Sprinting = sprinting;
-            wallMovement.Context.Sprinting = sprinting;
         }
 
         public void SetWalking(bool walking)
@@ -108,11 +75,6 @@ namespace Game.Components.Movement
                 case MovementMode.Humanoid:
                     this.Log().Debug("Switch to default movement!", nameof(SwitchMode));
                     defaultMovement.Initialize();
-                    break;
-                
-                case MovementMode.WallMovement:
-                    this.Log().Debug("Switch to wall movement",nameof(SwitchMode));
-                    wallMovement.Initialize();
                     break;
             }
         }
@@ -131,18 +93,6 @@ namespace Game.Components.Movement
             context.MovementDirection = Vector3.zero;
             context.Accelerate        = false;
             context.Jumping           = false;
-        }
-
-        private void WallMovementUpdate()
-        {
-            var context = wallMovement.Context;
-            
-            context.MovementDirection = (myTransform.forward * movementInput.y + myTransform.right * movementInput.x).normalized;
-            
-            wallMovement.Run(context, Time.fixedTime);
-            
-            // Consume state
-            context.MovementDirection = Vector3.zero;
         }
     }
 }
