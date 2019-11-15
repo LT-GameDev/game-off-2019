81ea914ad2b2e1b9a59aee06501d90510f02fed7
Romans <50585282+rkononovs@users.noreply.github.com>
2019-11-15 00:01:31 +0000

    Incorporated Sliding Door in door opener script, made them in functions.

--- Assets/Game/Scripts/Components/Activatables/DoorOpener.cs
+++ Assets/Game/Scripts/Components/Activatables/DoorOpener.cs
@@ -7,7 +7,9 @@ namespace Game.Components.Activatables
 {
     public class DoorOpener : Activatable, IInteractable
     {
+        Vector3 defaultDoorPosition;
         public float smooth = 2f;
+        public float doorOpenDistance = 2f;
         public float doorOpenAngle = 90f;
         public float doorCloseAngle = 0f;
         public bool isOpen = false;
@@ -30,30 +32,46 @@ namespace Game.Components.Activatables
             isOpen = state;
         }
 
+        private void OpenStandardDoor()
+        {
+            if (isOpen)
+            {
+                Quaternion targetRotation = Quaternion.Euler(0, doorOpenAngle, 0); // // Calculate how much is rotation to ending position
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+            }
+            else
+            {
+                Quaternion targetRotation2 = Quaternion.Euler(0, doorCloseAngle, 0); // Calculate how much is rotation to starting position
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, smooth * Time.deltaTime);
+            }
+        }
 
+        private void OpenSlidingDoor()
+        {
+            if (isOpen)
+            {
+                float targetEndingPositionZ = Mathf.Lerp(transform.localPosition.z, defaultDoorPosition.z + doorOpenDistance, Time.deltaTime * smooth); // Calculate how much should door move
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, targetEndingPositionZ);
+            }
+            else
+            {
+                float targetStartingPositionZ = Mathf.Lerp(transform.localPosition.z, defaultDoorPosition.z, Time.deltaTime * smooth); // Calculate how much should door move back
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, targetStartingPositionZ);
+            }
+        }
+        void Start()
+        {
+            defaultDoorPosition = transform.localPosition;
+        }
         void Update()
         {
             if (gameObject.tag == "SlidingDoor") // Opens GameObjects with tag Sliding Door
             {
-                if (isOpen)
-                {
-                }
-                else
-                {
-                }
+                OpenSlidingDoor();
             }
             else // Opens GameObjects with other tags (Basic Door)
             {
-                if (isOpen)
-                {
-                    Quaternion targetRotation = Quaternion.Euler(0, doorOpenAngle, 0);
-                    transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
-                }
-                else
-                {
-                    Quaternion targetRotation2 = Quaternion.Euler(0, doorCloseAngle, 0);
-                    transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, smooth * Time.deltaTime);
-                }
+                OpenStandardDoor();
             }
         }
     }
