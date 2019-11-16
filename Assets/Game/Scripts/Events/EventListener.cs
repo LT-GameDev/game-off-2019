566b5391ff161ba0966aeb039d2256c52ca67aa1
Cem Kucukhasanoglu <kucukhasanoglu.cem@gmail.com>
2019-11-16 16:05:54 +0300

    import game events

---
+++ Assets/Game/Scripts/Events/EventListener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game.Events
+{
+    public class EventListener : MonoBehaviour
+    {
+        [SerializeField] private GameEvent gameEvent;
+        [SerializeField] private HandleGameEvent handlers;
+
+
+        private void OnEnable()
+        {
+            gameEvent.AddListener(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.RemoveListener(this);
+        }
+
+
+        public void HandleEvent(GameEvent gameEvent)
+        {
+            handlers.Invoke(gameEvent);
+        }
+
+
+        [Serializable]
+        private class HandleGameEvent : UnityEvent<GameEvent> { }
+    }
+}
