90c0044e34599da0efb3b580b3df6c1662246d17
Cem Kucukhasanoglu <kucukhasanoglu.cem@gmail.com>
2019-11-16 16:08:46 +0300

    main menu scene

---
+++ Assets/Game/Scripts/Utility/SelectSelectable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Utility
+{
+    public class SelectSelectable : MonoBehaviour
+    {
+        [SerializeField] private Selectable selectable;
+        
+        private void OnEnable()
+        {
+            selectable.Select();
+        }
+    }
+}
