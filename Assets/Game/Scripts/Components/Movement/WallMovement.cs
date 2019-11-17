d9597923c47e94eb8eda93edabaa72e6e639c300
Cem Kucukhasanoglu <kucukhasanoglu.cem@gmail.com>
2019-11-17 22:56:35 +0300

    jumping off the wall

--- Assets/Game/Scripts/Components/Movement/WallMovement.cs
+++ Assets/Game/Scripts/Components/Movement/WallMovement.cs
@@ -15,6 +15,7 @@ namespace Game.Components.Movement
         [SerializeField] private float climbVelocity;
         [SerializeField] private float fallAcceleration;
         [SerializeField] private float rotationSpeed;
+        [SerializeField] private float jumpPower;
         
         private PlayerMovementContext movementContext;
         private Vector3 previousNormal;
@@ -32,12 +33,26 @@ namespace Game.Components.Movement
             
             previousNormal = movementContext.wallNormal;
             upwardsVector  = climbVelocity;
+
+            movementContext.jumpOff = false;
         }
 
         public void Run(float deltaTime)
         {
             if (movementContext.falling)
+            {
                 return;
+            }
+
+            if (movementContext.jumpOff)
+            {
+                var groundPlacneVelocity = Vector3.Scale(new Vector3(1, 0, 1), movementContext.body.velocity);
+                var jumpOffUpRotation    = Quaternion.LookRotation(groundPlacneVelocity.normalized, Vector3.up);
+            
+                movementContext.meshRoot.rotation = Quaternion.Lerp(movementContext.meshRoot.rotation, jumpOffUpRotation, rotationSpeed * deltaTime);
+                
+                return;
+            }
 
             if (!movementContext.sprint)
             {
@@ -50,6 +65,18 @@ namespace Game.Components.Movement
             if (!result.success)
             {
                 movementContext.falling = true;
+                return;
+            }
+
+            if (movementContext.jump)
+            {
+                var velocity          = movementContext.body.velocity;
+                var velocityDirection = (movementContext.wallNormal + velocity.normalized + 0.5f * climbVelocity * Vector3.up).normalized;
+                
+                movementContext.body.AddForce(velocityDirection * jumpPower, ForceMode.VelocityChange);
+                movementContext.body.useGravity = true;
+
+                movementContext.jumpOff = true;
                 return;
             }
 
--- Assets/Game/Scripts/Models/Movement/PlayerMovementContext.cs
+++ Assets/Game/Scripts/Models/Movement/PlayerMovementContext.cs
@@ -4,6 +4,7 @@ namespace Game.Models.Movement
 {
     public class PlayerMovementContext : DefaultMovementContext
     {
+        public bool jumpOff;
         public bool falling;
         public Vector3 wallNormal;
         public float wallDistance;
